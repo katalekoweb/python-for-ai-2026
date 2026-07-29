4138483391d7dea9371f691d1459b2ffb1e2a488
Julião Kataleko <katalekoweb@gmail.com>
2026-07-29 16:15:03 +0100

    feat: variables and comments

--- hello.py
+++ hello.py
@@ -1,4 +1,21 @@
-import requests
+name = "Alice"
+age = 25
+is_student = True
 
-response = requests.get("https://api.github.com")
-print(response.status_code)
+# Comments - human readable texts
+# Snake case with lower case and undercore
+
+# Multiple lines comments
+"""
+This is a multi-line
+quote than can go on and on
+and on
+"""
+
+# Good commnets explain Why and not What
+
+first_name = "Alice" # This is the first name
+user_age = 25
+print(user_age)
+
+
