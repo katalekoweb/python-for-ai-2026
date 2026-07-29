81e86889c532d15178db67baa1b0da4f5c7985b4
Julião Kataleko <katalekoweb@gmail.com>
2026-07-29 17:19:49 +0100

    feat: for loop

--- hello.py
+++ hello.py
@@ -1,30 +1,6 @@
-# temperature = 26
-
-# if temperature >30:
-#     print("It's very hot!")
-# elif temperature > 25:
-#     print("It's hot!")
-# else:
-#     print("It's nice weather!")
-
-# score = 90
-
-# if score >= 90:
-#     print("A - Excellent!")
-# elif score >= 80:
-#     print("B - Good Job!")
-# elif score >= 70:
-#     print("C - Keep it up!")
-# else:
-#     print("F - Need Improvement")
-
-has_ticket = True
-age = 15
-
-if has_ticket:
-    if age >= 18:
-        print("Enjoy the movie!")
-    else:
-        print("Needs supervision")
-else:
-    print("Plase buy a ticket!")
+for i in range(1,7):
+    print(i)
+    
+# Count by 2s
+for i in range(0, 10, 2):
+    print(i)
